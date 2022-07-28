7ca162ce06bd536810507c94946a48d87692d71e
Mitya-Kupriianov <MityaBlow@gmail.com>
2022-07-28 18:06:59 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,9 @@
 const input = document.querySelector("#validation-input");
+const dataLength = +input.getAttribute("data-length");
 
 input.addEventListener("blur", (e) => {
   const length = e.target.value.length;
   if (length === 0) input.className = "";
-  else if (length === 6) input.className = "valid";
+  else if (length === dataLength) input.className = "valid";
   else input.className = "invalid";
 });
